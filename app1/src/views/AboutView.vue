d64b0600b6016ef6e38add929073ed7032bbe5fc
gilangvperdana <gilangvirga271@gmail.com>
2023-08-02 12:56:55 +0000

    try to change aboutMenu

--- app1/src/views/AboutView.vue
+++ app1/src/views/AboutView.vue
@@ -8,21 +8,21 @@ import { AppInfo } from '@/const.js'
     <template v-slot:main>
       <div class="q-pa-md">
         <q-list>
-          <q-item-label header class="text-indigo text-bold">Application Name</q-item-label>
+          <q-item-label header class="text-indigo text-bold">Applications Names</q-item-label>
           <q-item>
             <q-item-section>{{ AppInfo.AppName }}</q-item-section>
           </q-item>
 
           <q-separator spaced />
 
-          <q-item-label header class="text-indigo text-bold">Developer</q-item-label>
+          <q-item-label header class="text-indigo text-bold">Developers</q-item-label>
           <q-item>
             <q-item-section>{{ AppInfo.AppDeveloper }}</q-item-section>
           </q-item>
 
           <q-separator spaced />
 
-          <q-item-label header class="text-indigo text-bold">GitHub</q-item-label>
+          <q-item-label header class="text-indigo text-bold">GitHubs</q-item-label>
           <q-item>
             <q-item-section>{{ AppInfo.GithubLink }}</q-item-section>
           </q-item>
